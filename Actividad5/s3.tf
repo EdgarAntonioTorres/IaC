5b07b0628cca7f992da19cd52469a38b44d6fadb
EdgarAntonioTorres <tedgar562@gmail.com>
2025-09-23 16:31:53 -0600

    Actividad

--- Actividad5/s3.tf
+++ Actividad5/s3.tf
@@ -1,11 +1,11 @@
-resource "reandom_id" "bucket_suffix" {
+resource "random_id" "bucket_suffix" {
     byte_length = 7
 }
 
-resource "aws_s3_bucket" "josetoño_bucket" {
-    bucket = "josetoño-bucket-${random_id.bucket_suffix.hex}"
+resource "aws_s3_bucket" "joseedgar_bucket" {
+    bucket = "joseedgar-bucket-${random_id.bucket_suffix.hex}"
 }
 
 output "bucket_name" {
-    value = aws_s3_bucket.josetoño_bucket.bucket
+    value = aws_s3_bucket.joseedgar_bucket.bucket
 }
